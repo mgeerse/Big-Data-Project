17059d929b386ce6eef35c00945b3326cca914d9
Maarten Geerse <582297@student.inholland.nl>
2019-05-21 14:16:32 +0200

    Added stringAsFactor=false

--- inladen.R
+++ inladen.R
@@ -1,5 +1,5 @@
 # Data inladen
-ClimateData<-read.csv(climatedatafile, header=TRUE , sep=";")
+ClimateData<-read.csv(climatedatafile, header = TRUE, sep=";", stringsAsFactors = FALSE)
 print("Climate data loaded in.")
-HarvestData<-read.csv(harvestdatafile, header=TRUE , sep=",")
+HarvestData<-read.csv(harvestdatafile, header = TRUE, sep=",", stringsAsFactors = FALSE)
 print("Harvest data loaded in.")
