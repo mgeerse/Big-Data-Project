e7bfa5d3119a7322f4568a7f09ec8aa6460ace39
jaccoklouwer <jacco1994@hotmail.com>
2019-05-23 09:25:13 +0200

    data gecleaned

--- main.R
+++ main.R
@@ -11,7 +11,7 @@ runDataCleaner <- TRUE;
 # Main script file.
 # This script should be the only place to run from.
 # The first step for any user is to set the working directory below:
-setwd("")
+setwd("D:\documenten\Bigdata\Project\refactored scripts\Big-Data-Project")
 
 # Load in the user settings.
 source("usersettings.R")
