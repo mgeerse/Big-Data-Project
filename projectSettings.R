691a90eca93f43e45f36634c0b3339322be20693
Maarten Geerse <mgeerse@gmail.com>
2019-05-28 10:06:14 +0200

    snel

---
+++ projectSettings.R
@@ -0,0 +1,16 @@
+# Projectsettings file.
+
+# Set this to climatedata file name
+climatedatafile<-"klimaatdata1.csv"
+# Set this to harvestdata file name
+harvestdatafile<-"reformatted_data_with_sap_1.csv"
+
+## START Hypothesis Validation ##
+# T-test on or off:
+t_test_enabled<-TRUE
+# Chi-Square test on or off:
+chi_square_test_enabled<-FALSE
+# ANOVA test on or off:
+anova_test_enabled<-FALSE
+## END Hypothesis Validation ##
+
