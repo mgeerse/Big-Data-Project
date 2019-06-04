fb53c83e99f84bf70e9d07688f5da14f36365f91
Maarten Geerse <mgeerse@gmail.com>
2019-06-04 10:37:25 +0200

    Update dataLoader for new datafiles

--- dataLoader.R
+++ dataLoader.R
@@ -1,5 +1,13 @@
 # Data inladen
+
 ClimateData<-read.csv(climatedatafile, header = TRUE, fill=TRUE, sep=";", stringsAsFactors = FALSE)
 print("Climate data loaded in.")
+
 HarvestData<-read.csv(harvestdatafile, header = TRUE, fill=TRUE, sep=",", stringsAsFactors = FALSE)
 print("Harvest data loaded in.")
+
+IrrigationData1<-read.csv(irrigation_system_1, header = TRUE, fill=TRUE, sep=";", stringsAsFactors = FALSE)
+print("IrrigationData1 data loaded in.")
+
+IrrigationData2<-read.csv(irrigation_system_2, header = TRUE, fill=TRUE, sep=";", stringsAsFactors = FALSE)
+print("IrrigationData2 data loaded in.")
