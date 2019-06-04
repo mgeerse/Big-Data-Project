0894b0ff289942ff84a9327646cd759545ad5940
Maarten Geerse <mgeerse@gmail.com>
2019-06-04 11:57:54 +0200

    Added decimal parameter when loading in irrigationdata

--- dataLoader.R
+++ dataLoader.R
@@ -6,8 +6,8 @@ print("ClimateData loaded in.")
 HarvestData<-read.csv(harvestdatafile, header = TRUE, dec = ",", fill = FALSE, sep=";", stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
 print("HarvestData loaded in.")
 
-IrrigationData1<-read.csv(irrigation_system_1, header = TRUE, fill=TRUE, sep=";", stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
+IrrigationData1<-read.csv(irrigation_system_1, header = TRUE, dec = ",", fill=TRUE, sep=";", stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
 print("IrrigationData1 data loaded in.")
 
-IrrigationData2<-read.csv(irrigation_system_2, header = TRUE, fill=TRUE, sep=";", stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
+IrrigationData2<-read.csv(irrigation_system_2, header = TRUE, dec = ",", fill=TRUE, sep=";", stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
 print("IrrigationData2 data loaded in.")
