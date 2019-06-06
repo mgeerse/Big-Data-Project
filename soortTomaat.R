8d3c49f223a69f8c577a8ec7a38d5a3f76266dad
jaccoklouwer <jacco1994@hotmail.com>
2019-06-06 13:02:00 +0200

    Vier data sets van soorten tomaten

--- soortTomaat.R
+++ soortTomaat.R
@@ -14,13 +14,11 @@ finalmerge$correctWeight[finalmerge$plant>=500&finalmerge$plant<=511]<-182
 finalmerge$correctWeight[finalmerge$plant>=600&finalmerge$plant<=611]<-91
 finalmerge$correctWeight<-as.numeric(finalmerge$correctWeight)
 
-finalmerge$U<-ifelse(finalmerge$fruits*finalmerge$correctWeight<finalmerge$freshweight,0,1)
+finalmerge$U<-ifelse(finalmerge$fruits*finalmerge$correctWeight>finalmerge$freshweight,0,1)
 finalmerge$U<-as.factor(finalmerge$U)
 levels(finalmerge$U)<-c("low","high")
 
-D<-finalmerge[,-c(40, 37, 38)]
-View(finalmerge)
-length(finalmerge)
-names(finalmerge)
-summary(D)
-
+HarvestBig <-finalmerge[finalmerge$soort == "big", ]
+HarvestCherry <- finalmerge[finalmerge$soort == "cherry", ]
+HarvestSmall <- finalmerge[finalmerge$soort == "small", ]
+HarvestPrunaxx <- finalmerge[finalmerge$soort == "prunaxx", ]
