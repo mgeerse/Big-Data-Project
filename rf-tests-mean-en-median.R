efd8c17859169bc57e38df48bbe9ed787745bdb5
jaccoklouwer <jacco1994@hotmail.com>
2019-06-12 11:18:52 +0200

    median er uitgehaald

--- rf-tests-mean-en-median.R
+++ rf-tests-mean-en-median.R
@@ -1,207 +1,134 @@
 # random Forest
-
-BigMedian<-HarvestBigMedianMerged[,-c(1,5)]
-PrunaxxMedian<-HarvestPrunaxxMedianMerged[,-c(1,5)]
-SmallMedian<-HarvestSmallMedianMerged[,-c(1,5)]
-CherryMedian<-HarvestCherryMedianMerged[,-c(1,5)]
+set.seed(1)
 
 BigMean<-HarvestBigMeanMerged[,-c(1,5)]
 PrunaxxMean<-HarvestPrunaxxMeanMerged[,-c(1,5)]
 SmallMean<-HarvestSmallMeanMerged[,-c(1,5)]
 CherryMean<-HarvestCherryMeanMerged[,-c(1,5)]
 
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=BigMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,BigMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~BigMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=PrunaxxMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,PrunaxxMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~PrunaxxMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=SmallMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,SmallMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~SmallMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=CherryMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,CherryMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~CherryMedian$averageWeight,ylab="probability",col=c("red","green"))
-
+### weight
 windows()
 rfModel<-randomForest(
 		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=BigMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
+rfProbs<-predict(rfModel,type="prob")
 AUCrf<-colAUC(rfProbs,BigMean$averageWeight,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~BigMean$averageWeight,ylab="probability",col=c("red","green"))
 
 windows()
 rfModel<-randomForest(
 		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=PrunaxxMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
+rfProbs<-predict(rfModel,type="prob")
 AUCrf<-colAUC(rfProbs,PrunaxxMean$averageWeight,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~PrunaxxMean$averageWeight,ylab="probability",col=c("red","green"))
 
 windows()
 rfModel<-randomForest(
 		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=SmallMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
+rfProbs<-predict(rfModel,type="prob")
 AUCrf<-colAUC(rfProbs,SmallMean$averageWeight,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~SmallMean$averageWeight,ylab="probability",col=c("red","green"))
 
 windows()
 rfModel<-randomForest(
 		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=CherryMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
+rfProbs<-predict(rfModel,type="prob")
 AUCrf<-colAUC(rfProbs,CherryMean$averageWeight,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~CherryMean$averageWeight,ylab="probability",col=c("red","green"))
 
-###
+### Brix
 
 windows()
 rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=BigMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,BigMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~BigMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=PrunaxxMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,PrunaxxMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~PrunaxxMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=SmallMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,SmallMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~SmallMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
-		data=CherryMedian,
-		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,CherryMedian$averageWeight,alg="ROC",plot=T)
-AUCrf
-varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~CherryMedian$averageWeight,ylab="probability",col=c("red","green"))
-
-windows()
-rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		averageBrix~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=BigMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,BigMean$averageWeight,alg="ROC",plot=T)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,BigMean$averageBrix,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~BigMean$averageWeight,ylab="probability",col=c("red","green"))
 
 windows()
 rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		averageBrix~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=PrunaxxMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,PrunaxxMean$averageWeight,alg="ROC",plot=T)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,PrunaxxMean$averageBrix,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~PrunaxxMean$averageWeight,ylab="probability",col=c("red","green"))
 
 windows()
 rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		averageBrix~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=SmallMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,SmallMean$averageWeight,alg="ROC",plot=T)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,SmallMean$averageBrix,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
-windows()
-boxplot(rfProbs~SmallMean$averageWeight,ylab="probability",col=c("red","green"))
 
 windows()
 rfModel<-randomForest(
-		averageWeight~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		averageBrix~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=CherryMean,
 		ntree=500,importance=TRUE)
-rfProbs<-predict(rfModel,type="prob")[,"Low yield"]
-AUCrf<-colAUC(rfProbs,CherryMean$averageWeight,alg="ROC",plot=T)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,CherryMean$averageBrix,alg="ROC",plot=T)
 AUCrf
 varImpPlot(rfModel,sort=TRUE)
+
+### Fruits
+
 windows()
-boxplot(rfProbs~CherryMean$averageWeight,ylab="probability",col=c("red","green"))
+rfModel<-randomForest(
+		averageFruits~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		data=BigMean,
+		ntree=500,importance=TRUE)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,BigMean$averageFruits,alg="ROC",plot=T)
+AUCrf
+varImpPlot(rfModel,sort=TRUE)
+
+windows()
+rfModel<-randomForest(
+		averageFruits~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		data=PrunaxxMean,
+		ntree=500,importance=TRUE)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,PrunaxxMean$averageFruits,alg="ROC",plot=T)
+AUCrf
+varImpPlot(rfModel,sort=TRUE)
+
+windows()
+rfModel<-randomForest(
+		averageFruits~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		data=SmallMean,
+		ntree=500,importance=TRUE)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,SmallMean$averageFruits,alg="ROC",plot=T)
+AUCrf
+varImpPlot(rfModel,sort=TRUE)
+
+windows()
+rfModel<-randomForest(
+		averageFruits~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
+		data=CherryMean,
+		ntree=500,importance=TRUE)
+rfProbs<-predict(rfModel,type="prob")
+AUCrf<-colAUC(rfProbs,CherryMean$averageFruits,alg="ROC",plot=T)
+AUCrf
+varImpPlot(rfModel,sort=TRUE)
 
 ###
--- soortTomaat.R
+++ soortTomaat.R
@@ -37,17 +37,6 @@ rownames(HarvestPrunaxx) <- c()
 HarvestPrunaxx$soort<-NULL
 HarvestPrunaxx$weightperfruit <- NULL
 
-HarvestBigMedian<-aggregate(x = HarvestBig[c("brix", "fruits", "diameter", "freshweight")],
-    FUN = MedianNoNA, by = list(date = HarvestBig$date))
-
-HarvestPrunaxxMedian<-aggregate(x = HarvestPrunaxx[c("brix", "fruits", "diameter", "freshweight")],
-    FUN = MedianNoNA, by = list(date = HarvestPrunaxx$date))
-
-HarvestSmallMedian<-aggregate(x = HarvestSmall[c("brix", "fruits", "diameter", "freshweight")],
-    FUN = MedianNoNA, by = list(date = HarvestSmall$date))
-
-HarvestCherryMedian<-aggregate(x = HarvestCherry[c("brix", "fruits", "diameter", "freshweight")],
-    FUN = MedianNoNA, by = list(date = HarvestCherry$date))
 
 HarvestBigMean<-aggregate(x = HarvestBig[c("brix", "fruits", "diameter", "freshweight")],
     FUN = MeanNoNA, by = list(date = HarvestBig$date))
@@ -61,30 +50,11 @@ HarvestSmallMean<-aggregate(x = HarvestSmall[c("brix", "fruits", "diameter", "fr
 HarvestCherryMean<-aggregate(x = HarvestCherry[c("brix", "fruits", "diameter", "freshweight")],
     FUN = MeanNoNA, by = list(date = HarvestCherry$date))
 
-HarvestBigMedianMerged <-merge(HarvestBigMedian, system1, by.x = "date", by.y = "date")
-HarvestPrunaxxMedianMerged <-merge(HarvestPrunaxxMedian, system1, by.x = "date", by.y = "date")
-HarvestSmallMedianMerged <-merge(HarvestSmallMedian, system2, by.x = "date", by.y = "date")
-HarvestCherryMedianMerged <-merge(HarvestCherryMedian, system2, by.x = "date", by.y = "date")
 HarvestBigMeanMerged <-merge(HarvestBigMean, system1, by.x = "date", by.y = "date")
 HarvestPrunaxxMeanMerged <-merge(HarvestPrunaxxMean, system1, by.x = "date", by.y = "date")
 HarvestSmallMeanMerged <-merge(HarvestSmallMean, system2, by.x = "date", by.y = "date")
 HarvestCherryMeanMerged <-merge(HarvestCherryMean, system2, by.x = "date", by.y = "date")
 
-HarvestBigMedianMerged$averageWeight<-ifelse(HarvestBigMedianMerged$freshweight<mean(HarvestBigMedianMerged$freshweight),0,1)
-HarvestBigMedianMerged$averageWeight<-as.factor(HarvestBigMedianMerged$averageWeight)
-levels(HarvestBigMedianMerged$averageWeight)<-c("Low yield", "high yield")
-
-HarvestPrunaxxMedianMerged$averageWeight<-ifelse(HarvestPrunaxxMedianMerged$freshweight<mean(HarvestPrunaxxMedianMerged$freshweight),0,1)
-HarvestPrunaxxMedianMerged$averageWeight<-as.factor(HarvestPrunaxxMedianMerged$averageWeight)
-levels(HarvestPrunaxxMedianMerged$averageWeight)<-c("Low yield", "high yield")
-
-HarvestSmallMedianMerged$averageWeight<-ifelse(HarvestSmallMedianMerged$freshweight<mean(HarvestSmallMedianMerged$freshweight),0,1)
-HarvestSmallMedianMerged$averageWeight<-as.factor(HarvestSmallMedianMerged$averageWeight)
-levels(HarvestSmallMedianMerged$averageWeight)<-c("Low yield", "high yield")
-
-HarvestCherryMedianMerged$averageWeight<-ifelse(HarvestCherryMedianMerged$freshweight<mean(HarvestCherryMedianMerged$freshweight),0,1)
-HarvestCherryMedianMerged$averageWeight<-as.factor(HarvestCherryMedianMerged$averageWeight)
-levels(HarvestCherryMedianMerged$averageWeight)<-c("Low yield", "high yield")
 
 ###
 HarvestBigMeanMerged$averageWeight<-ifelse(HarvestBigMeanMerged$freshweight<mean(HarvestBigMeanMerged$freshweight),0,1)
@@ -105,23 +75,6 @@ levels(HarvestCherryMeanMerged$averageWeight)<-c("Low yield", "High yield")
 
 ####
 ####
-HarvestBigMedianMerged$averageBrix<-ifelse(HarvestBigMedianMerged$brix<mean(HarvestBigMedianMerged$brix),0,1)
-HarvestBigMedianMerged$averageBrix<-as.factor(HarvestBigMedianMerged$averageBrix)
-levels(HarvestBigMedianMerged$averageBrix)<-c("Low", "High")
-
-HarvestPrunaxxMedianMerged$averageBrix<-ifelse(HarvestPrunaxxMedianMerged$brix<mean(HarvestPrunaxxMedianMerged$brix),0,1)
-HarvestPrunaxxMedianMerged$averageBrix<-as.factor(HarvestPrunaxxMedianMerged$averageBrix)
-levels(HarvestPrunaxxMedianMerged$averageBrix)<-c("Low", "High")
-
-HarvestSmallMedianMerged$averageBrix<-ifelse(HarvestSmallMedianMerged$brix<mean(HarvestSmallMedianMerged$brix),0,1)
-HarvestSmallMedianMerged$averageBrix<-as.factor(HarvestSmallMedianMerged$averageBrix)
-levels(HarvestSmallMedianMerged$averageBrix)<-c("Low", "High")
-
-HarvestCherryMedianMerged$averageBrix<-ifelse(HarvestCherryMedianMerged$brix<mean(HarvestCherryMedianMerged$brix),0,1)
-HarvestCherryMedianMerged$averageBrix<-as.factor(HarvestCherryMedianMerged$averageBrix)
-levels(HarvestCherryMedianMerged$averageBrix)<-c("Low", "High")
-
-###
 HarvestBigMeanMerged$averageBrix<-ifelse(HarvestBigMeanMerged$brix<mean(HarvestBigMeanMerged$brix),0,1)
 HarvestBigMeanMerged$averageBrix<-as.factor(HarvestBigMeanMerged$averageBrix)
 levels(HarvestBigMeanMerged$averageBrix)<-c("Low", "High")
@@ -140,23 +93,6 @@ levels(HarvestCherryMeanMerged$averageBrix)<-c("Low", "High")
 
 ####
 ####
-HarvestBigMedianMerged$averageFruits<-ifelse(HarvestBigMedianMerged$fruits<mean(HarvestBigMedianMerged$fruits),0,1)
-HarvestBigMedianMerged$averageFruits<-as.factor(HarvestBigMedianMerged$averageFruits)
-levels(HarvestBigMedianMerged$averageFruits)<-c("Low", "High")
-
-HarvestPrunaxxMedianMerged$averageFruits<-ifelse(HarvestPrunaxxMedianMerged$fruits<mean(HarvestPrunaxxMedianMerged$fruits),0,1)
-HarvestPrunaxxMedianMerged$averageFruits<-as.factor(HarvestPrunaxxMedianMerged$averageFruits)
-levels(HarvestPrunaxxMedianMerged$averageFruits)<-c("Low", "High")
-
-HarvestSmallMedianMerged$averageFruits<-ifelse(HarvestSmallMedianMerged$fruits<mean(HarvestSmallMedianMerged$fruits),0,1)
-HarvestSmallMedianMerged$averageFruits<-as.factor(HarvestSmallMedianMerged$averageFruits)
-levels(HarvestSmallMedianMerged$averageFruits)<-c("Low", "High")
-
-HarvestCherryMedianMerged$averageFruits<-ifelse(HarvestCherryMedianMerged$fruits<mean(HarvestCherryMedianMerged$fruits),0,1)
-HarvestCherryMedianMerged$averageFruits<-as.factor(HarvestCherryMedianMerged$averageFruits)
-levels(HarvestCherryMedianMerged$averageFruits)<-c("Low", "High")
-
-###
 HarvestBigMeanMerged$averageFruits<-ifelse(HarvestBigMeanMerged$fruits<mean(HarvestBigMeanMerged$fruits),0,1)
 HarvestBigMeanMerged$averageFruits<-as.factor(HarvestBigMeanMerged$averageFruits)
 levels(HarvestBigMeanMerged$averageFruits)<-c("Low", "High")
