3ba528dc8804aaa6e1cfca63db11706099d38092
jaccoklouwer <jacco1994@hotmail.com>
2019-06-12 12:17:59 +0200

    titel toegvoegd aan de diagrammen

--- rf-tests-mean-en-median.R
+++ rf-tests-mean-en-median.R
@@ -13,9 +13,9 @@ rfModel<-randomForest(
 		data=BigMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,BigMean$averageWeight,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,BigMean$averageWeight,alg="ROC",plot=T)[,"High Yield"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average weigth of a big tomato")
 
 windows()
 rfModel<-randomForest(
@@ -23,9 +23,9 @@ rfModel<-randomForest(
 		data=PrunaxxMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,PrunaxxMean$averageWeight,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,PrunaxxMean$averageWeight,alg="ROC",plot=T)[,"High Yield"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average weigth of a prunaxx tomato")
 
 windows()
 rfModel<-randomForest(
@@ -33,9 +33,9 @@ rfModel<-randomForest(
 		data=SmallMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,SmallMean$averageWeight,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,SmallMean$averageWeight,alg="ROC",plot=T)[,"High Yield"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average weigth of a small tomato")
 
 windows()
 rfModel<-randomForest(
@@ -43,21 +43,20 @@ rfModel<-randomForest(
 		data=CherryMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,CherryMean$averageWeight,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,CherryMean$averageWeight,alg="ROC",plot=T)[,"High Yield"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average weigth of a cherry tomato")
 
 ### Brix
-
 windows()
 rfModel<-randomForest(
 		averageBrix~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=BigMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,BigMean$averageBrix,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,BigMean$averageBrix,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average brix of a big tomato")
 
 windows()
 rfModel<-randomForest(
@@ -65,9 +64,9 @@ rfModel<-randomForest(
 		data=PrunaxxMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,PrunaxxMean$averageBrix,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,PrunaxxMean$averageBrix,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average brix of a prunax tomato")
 
 windows()
 rfModel<-randomForest(
@@ -75,9 +74,9 @@ rfModel<-randomForest(
 		data=SmallMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,SmallMean$averageBrix,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,SmallMean$averageBrix,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average brix of a small tomato")
 
 windows()
 rfModel<-randomForest(
@@ -85,21 +84,20 @@ rfModel<-randomForest(
 		data=CherryMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,CherryMean$averageBrix,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,CherryMean$averageBrix,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average brix of a cherry tomato")
 
 ### Fruits
-
 windows()
 rfModel<-randomForest(
 		averageFruits~room.temperature.inside+air.humidity+CO2.amount+radiation.outside+plant.temperature.big+plant.temperature.small+Average.EC+Average.Flow+Average.pH+Sum.LitersPerM2+Sum.Deviation+Sum.TotalLiters+Sum.Usage+drain.percentage+EC+pH+drain.water.amount+absorb.water.amount,
 		data=BigMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,BigMean$averageFruits,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,BigMean$averageFruits,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average number of fruits of a big tomato")
 
 windows()
 rfModel<-randomForest(
@@ -107,9 +105,9 @@ rfModel<-randomForest(
 		data=PrunaxxMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,PrunaxxMean$averageFruits,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,PrunaxxMean$averageFruits,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average number of fruits of a prunaxx tomato")
 
 windows()
 rfModel<-randomForest(
@@ -117,9 +115,9 @@ rfModel<-randomForest(
 		data=SmallMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,SmallMean$averageFruits,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,SmallMean$averageFruits,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average number of fruits of a small tomato")
 
 windows()
 rfModel<-randomForest(
@@ -127,8 +125,7 @@ rfModel<-randomForest(
 		data=CherryMean,
 		ntree=500,importance=TRUE)
 rfProbs<-predict(rfModel,type="prob")
-AUCrf<-colAUC(rfProbs,CherryMean$averageFruits,alg="ROC",plot=T)
+AUCrf<-colAUC(rfProbs,CherryMean$averageFruits,alg="ROC",plot=T)[,"High"]
 AUCrf
-varImpPlot(rfModel,sort=TRUE)
-
+varImpPlot(rfModel,sort=TRUE, main = "variables that have influance on the average number of fruits of a cherry tomato")
 ###
