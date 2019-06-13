584af074d87f290495699b36784723e6c190ca91
Maarten Geerse <582297@student.inholland.nl>
2019-06-13 09:59:58 +0200

    Data per irrigationsystem

--- dataMerger.R
+++ dataMerger.R
@@ -3,3 +3,11 @@
 
 system1<-merge(ClimateDataPerDay, IrrigationData1, by.x = "date", by.y = "date")
 system2<-merge(ClimateDataPerDay, IrrigationData2, by.x = "date", by.y = "date")
+
+HarvestDataToMerge<-completeHarvestData(HarvestData)
+
+system1<-merge(system1, HarvestDataToMerge, by.x="date", by.y="date")
+system2<-merge(system2, HarvestDataToMerge, by.x="date", by.y="date")
+
+trainingData1<-rbind(system1[system1$plant>=400&system1$plant<=411,], system1[system1$plant>=600&system1$plant<=611,])
+trainingData2<-rbind(system1[system2$plant>=300&system2$plant<=311,], system2[system1$plant>=500&system2$plant<=511,])
